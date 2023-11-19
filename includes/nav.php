270fe459dacb075dcb0541a6ce6431b9e29882f2
Imane Eddouni <66179325+imane20112018@users.noreply.github.com>
2023-11-20 00:14:09 +0100

    creating master page and database

---
+++ includes/nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-light bg-primary">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1 text-center w-100 text-white">Todo App</span>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include_once 'includes/nav.php'; ?>
+    <div class="container">
+        <section>
+            
+        </section>
+    </div>
+</body>
+
+</html>
